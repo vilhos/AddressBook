342bd4fe1065baad77665270ca9414d196ad6495
Vilen <abyancz@gmail.com>
2022-03-31 23:31:07 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace AddressBook_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.firstname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phonenumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.firstname,
+            this.phonenumber,
+            this.email,
+            this.address,
+            this.Id});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(754, 289);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // firstname
+            // 
+            this.firstname.HeaderText = "First Name";
+            this.firstname.MinimumWidth = 6;
+            this.firstname.Name = "firstname";
+            this.firstname.ReadOnly = true;
+            this.firstname.Width = 150;
+            // 
+            // phonenumber
+            // 
+            this.phonenumber.HeaderText = "Phone Number";
+            this.phonenumber.MinimumWidth = 6;
+            this.phonenumber.Name = "phonenumber";
+            this.phonenumber.ReadOnly = true;
+            this.phonenumber.Width = 150;
+            // 
+            // email
+            // 
+            this.email.HeaderText = "Email";
+            this.email.MinimumWidth = 6;
+            this.email.Name = "email";
+            this.email.Width = 155;
+            // 
+            // address
+            // 
+            this.address.HeaderText = "Address";
+            this.address.MinimumWidth = 6;
+            this.address.Name = "address";
+            this.address.Width = 245;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Show All Contacts";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ShowAllContacts);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(183, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "View Single Contact";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ViewSingleContact);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(329, 374);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 48);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Create Contact";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.CreateContact);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(476, 374);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 48);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Edit Contact";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.EditContact);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(624, 374);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 48);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Delete Contact";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.DeleteContact);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_FullName);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(221, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_Email);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(369, 34);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(145, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_Number);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(521, 34);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(246, 22);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_Address);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            this.Id.Visible = false;
+            this.Id.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phonenumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace AddressBook_WinForm
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string DB = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\Address_Book\Database.mdf;Integrated Security=True";
+            connection = new SqlConnection(DB);
+            connection.Open();
+        }
+
+        private void ShowAllContacts(object sender, EventArgs e)
+        {
+            Func();
+        }
+
+        private void Func()
+        {
+            SqlDataReader sqlReader = null;
+            SqlCommand command = new SqlCommand("SELECT * FROM [Contacts]", connection);
+            sqlReader = command.ExecuteReader();
+            dataGridView1.Rows.Clear();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            while (sqlReader.Read())
+            {
+                dataGridView1.Rows.Add((string)sqlReader["Name"], (string)sqlReader["Number"], (string)sqlReader["Email"], (string)sqlReader["Address"], sqlReader["Id"]);
+            }
+            sqlReader.Close();
+        }
+
+        private void ViewSingleContact(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            SqlCommand command = new SqlCommand("SELECT * FROM [Contacts] WHERE Name LIKE '%"+textBox1.Text+"%'", connection);
+            command.Parameters.AddWithValue("Name", textBox1.Text);
+            SqlDataReader sqlReader = command.ExecuteReader();
+            while (sqlReader.Read())
+            {
+                dataGridView1.Rows.Add((string)sqlReader["Name"], (string)sqlReader["Number"], (string)sqlReader["Email"], (string)sqlReader["Address"], sqlReader["Id"]);
+            }
+            sqlReader.Close();
+        }
+
+        private void CreateContact(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(textBox1.Text) && !textBox1.Text.Any(char.IsNumber) && !string.IsNullOrEmpty(textBox2.Text) && !textBox2.Text.Any(c => (c < '0' || c > '9') && c != ' '))
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO [Contacts] (Name, Number, Email, Address) VALUES(@Name, @Number, @Email, @Address)", connection);
+                command.Parameters.AddWithValue("Name", textBox1.Text);
+                command.Parameters.AddWithValue("Number", textBox2.Text);
+                command.Parameters.AddWithValue("Email", textBox3.Text);
+                command.Parameters.AddWithValue("Address", textBox4.Text);
+                command.ExecuteNonQuery();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Value");
+                return;
+            }
+            Func();
+        }
+
+        private void EditContact(object sender, EventArgs e)
+        {
+            var id = dataGridView1.SelectedCells[4].Value;
+            if (!string.IsNullOrEmpty(textBox1.Text) && !textBox1.Text.Any(char.IsNumber) && !string.IsNullOrEmpty(textBox2.Text) && !textBox2.Text.Any(c => (c < '0' || c > '9') && c != ' '))
+            {
+                SqlCommand command = new SqlCommand("UPDATE [Contacts] SET [Name]=@Name, [Number]=@Number, [Email]=@Email, [Address]=@Address WHERE [Id]=@Id", connection);
+                command.Parameters.AddWithValue("Name", textBox1.Text);
+                command.Parameters.AddWithValue("Number", textBox2.Text);
+                command.Parameters.AddWithValue("Email", textBox3.Text);
+                command.Parameters.AddWithValue("Address", textBox4.Text);
+                command.Parameters.AddWithValue("Id", id);
+                command.ExecuteNonQuery();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Value");
+                return;
+            }
+
+            Func();
+        }
+
+        private void DeleteContact(object sender, EventArgs e)
+        {
+            string name = (string)dataGridView1.SelectedCells[0].Value;
+            SqlCommand command = new SqlCommand("DELETE FROM [Contacts] WHERE [Name]=@Name", connection);
+            command.Parameters.AddWithValue("Name", name);
+            command.ExecuteNonQuery();
+
+            Func();
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = (string)dataGridView1.SelectedCells[0].Value;
+            textBox2.Text = (string)dataGridView1.SelectedCells[1].Value;
+            textBox3.Text = (string)dataGridView1.SelectedCells[2].Value;
+            textBox4.Text = (string)dataGridView1.SelectedCells[3].Value;
+        }
+
+        private void textBox1_FullName(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_Email(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_Number(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_Address(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
